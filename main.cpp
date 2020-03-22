39eedd42cb67b0f03bb3e9e3d24e7afbca37f518
B03233 <B03233@B03233>
2020-03-22 09:18:46 +0800

    vscode

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+
+
+int main(int argc, char const *argv[])
+{
+    cv::Mat src = imread("C://Users/B03233.LUSTERINC//Desktop//total.bmp",2 | 4);
+    std::cout<<"hello vs"<<std::endl;
+    return 0;
+}
